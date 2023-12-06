b386efeeb37842f36dfa73a86f760b51968905a2
srberard <stephen.berard@outlook.com>
2023-12-06 11:34:01 +0100

    Initial work for kickstarting Atym

--- samples/blinky/main.c
+++ samples/blinky/main.c
@@ -4,18 +4,18 @@
 
 #include <stdio.h>
 
-#include "bi-inc/nubix_api.h"
+#include "wa-inc/timer_wasm_app.h"
 
 void update_led(user_timer_t timer) {
-  nbx_toggle_led();
-  printf(".");
+  //nbx_toggle_led();
+  printf(".\n");
 }
 
 // Nubix container entrypoint
-void nubix_init() {
+void on_init() {
   user_timer_t timer;
   printf("Blinky app initializing.\n");
 
-  timer = nbx_timer_create(500, true, false, update_led);
-  nbx_timer_restart(timer, 500);
+  timer = api_timer_create(500, true, false, update_led);
+  api_timer_restart(timer, 500);
 }
