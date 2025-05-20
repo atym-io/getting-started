d4eb04852a16bdb325e61e69f3ee0f6aec0ac86d
Knox Lively <knox@atym.io>
2025-05-20 13:43:52 -0600

    Update main.c (#12)
    Converted demo to ASCII from Unicode

--- samples/hello-world/main.c
+++ samples/hello-world/main.c
@@ -1,23 +1,18 @@
 /*
  * Copyright (C) 2025 Atym Incorporated. All rights reserved.
  */
+ 
 #include <stdio.h>
-#include <locale.h>
 
 // Application entry point
-int main() 
-{
-    // Set the locale to support Unicode characters
-    setlocale(LC_ALL, "");
-    
+int main()
+{  
     printf("\n\
-        ██     ██ ███████ ██████   █████  ███████ ███████ ███████ ███    ███ ██████  ██   ██    ██ \n\
-        ██     ██ ██      ██   ██ ██   ██ ██      ██      ██      ████  ████ ██   ██ ██    ██  ██  \n\
-        ██  █  ██ █████   ██████  ███████ ███████ ███████ █████   ██ ████ ██ ██████  ██     ████   \n\
-        ██ ███ ██ ██      ██   ██ ██   ██      ██      ██ ██      ██  ██  ██ ██   ██ ██      ██    \n\
-         ███ ███  ███████ ██████  ██   ██ ███████ ███████ ███████ ██      ██ ██████  ███████ ██    \n\
-                                                                                                   \n\
-                                                                                 powered by Atym   ");
-    printf("\n\n Update v4\n\n");
+     _      _________  ___   ______________  ______  ____  __ \n\
+    | | /| / / __/ _ )/ _ | / __/ __/ __/  |/  / _ )/ /\\ \\/ / \n\
+    | |/ |/ / _// _  / __ |_\\ \\_\\ \\/ _// /|_/ / _  / /__\\  /  \n\
+    |__/|__/___/____/_/ |_/___/___/___/_/  /_/____/____//_/   \n\
+                                           powered by Atym  ");
+    printf("\n\n    Update v4\n\n");
     return 0;
 }
